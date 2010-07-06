1ca931a1de1c5dc18b6820f36273d7bd18080a7f
Chris Darroch <chris@chrisdarroch.com>
2010-07-06 22:53:34 +1000

    Rails 3 demo app to play around with arel and its join support.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '30f5708290bfa4339524b3e942bee34c50ce5bffc1289ac4a0ec76a683469a3f053980deb86c88489f6e05b7aa9968be9b9600565ff9a2fbc07cdaa429d980da'
